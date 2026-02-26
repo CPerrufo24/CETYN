b98a2bf768322e92e103e731af8ebf1de6f0aa36
Carlos Perrufo <perrufo.cyv@gmail.com>
2026-02-25 23:19:20 -0600

    Actualizacion de link

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Configuración de AOS (Animate On Scroll)
+AOS.init({
+    duration: 800,
+    easing: 'ease-out-quart',
+    delay: 50,
+    once: true,
+    mirror: false,
+    offset: 80,
+    anchorPlacement: 'top-bottom',
+});
